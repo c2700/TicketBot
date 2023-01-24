04ffe7eba39924761559cfa76be4045a4401c5f3
c2700 <rohan.philip.india@gmail.com>
2023-01-25 04:46:41 +0530

    changed input prompt

--- TicketBot.py
+++ TicketBot.py
@@ -10,7 +10,7 @@ import re
 def Login():
     if not path.exists(".hash"):
         print("no stored snow creds found. please input snow creds")
-        _instance = input("instance: ")
+        _instance = input("snow instance: ")
         _user = input("user: ")
         _password = input("password: ")
 
